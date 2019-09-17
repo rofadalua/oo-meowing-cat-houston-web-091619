4369e289e1530664255c2501bcc5cba95073d71a
ultimate-bell-1709 <rofadalua@hotmail.com>
2019-09-17 22:36:23 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,15 @@
-## code your solution here. 
+class Cat
+  def name=(name)
+    @name=(name)
+  end
+  
+  def name
+    @name=name
+  end
+  
+end
+
+maru=Cat.new 
+maru.name="Maru"
+puts maru.name 
+
